24c8dfff6539009dc11d55145040738b725c54de
Kunto Mustiko <kuntomustiko@gmail.com>
2020-02-03 11:59:22 +0700

    array of array

--- PELAJARAN/Pelajaran-Hari-6/array.js
+++ PELAJARAN/Pelajaran-Hari-6/array.js
@@ -101,8 +101,6 @@ console.log(heroes)
 // Splice, hapus data tertentu, yang lain geser, dan langsung mengubah data aslinya
 // Delete, akan menjadi undefined
 
-
-
 var words = ['Please', 'Save', 'Me', 'Now']
 // menghapus dari index ke 1, sebanyak 0 dan tambahkan "I", "NEED", "HELP"
 words.splice(1, 0, "I", "NEED", "HELP")
@@ -123,7 +121,6 @@ console.log(words)
 
 // Hapus mulai dari index 1 sampai terakhir
 words.splice(1)
-
 
 var words = ['Please', 'Save', 'Me', 'Now']
 
@@ -153,3 +150,41 @@ console.log(hariOne)
 // Copy data dari index 2-3
 var hariTwo = hari.slice(2, 4)
 console.log(hariTwo)
+
+////////////
+// MERGE //
+//////////
+
+var dayOne = ["Sunday", "Monday", "Tuesday"]
+var dayTwo = ['Thursday', 'Friday', 'Saturday']
+var dayThree = ['Wednesday']
+
+// kalau begini dia hanya menggabung dua array dengan membawa kotak array asalnya
+var dayOneTwo = [dayOne, dayTwo]
+console.log(dayOneTwo)
+
+// kalau begini dia menggabungkan dua array dengan hanya isi nya saja yang digabungkan tanpa membawa kotak kurung
+var dayOneTwoGabung = [...dayOne, ...dayTwo]
+console.log(dayOneTwoGabung)
+
+var dayOneTwoThreeGabung = [...dayOne, ...dayThree, ...dayTwo]
+console.log(dayOneTwoThreeGabung)
+
+
+/////////////////////
+// ARRAY OF ARRAY //
+///////////////////
+
+var persons = [
+    ['John', 44, 'assasin'],
+    ['Tom', 45, 'tank'],
+    ['Smith', 46, 'mage']
+]
+
+persons.push(
+    ['Brown', 47, 'Driver']
+)
+
+// menambah "single" pada index ke 2 dan di tambah pada posisi akhir
+persons[2].push("single")
+console.log(persons)
