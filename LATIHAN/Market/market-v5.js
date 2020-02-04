67a210b52e410726ae39b23313a07311eba51900
Kunto Mustiko <kuntomustiko@gmail.com>
2020-02-04 09:20:19 +0700

    perbaruan penamaan version market

--- LATIHAN/Market/market-v5.js
+++ LATIHAN/Market/market-v5.js
@@ -1,4 +1,16 @@
-// upgrade 3
+// upgrade V5
+/*
+
+Hanya boleh ada satu block do while untuk input jumlah qty setiap product.
+Gunakan for loop untuk menghitung total harga setiap barang.
+Gunakan for loop untuk menghitung total harga dari seluruh barang.
+Sekarang user tidak hanya bisa belanja namun ada 3 pilihan tambahan
+App akan memiliki 4 menu utama :
+0 . Menampilkan daftar buah
+1 . Menambah buah
+2 . Menghapus buah
+3 . Membeli buah
+Untuk setiap selesai menambah dan menghapus buah akan menampilkan daftar buah terbaru student akan mengulang kode yang sama untuk membuat list.*/
 // senin 03/02/2020
 /*
 # Upgrade :
