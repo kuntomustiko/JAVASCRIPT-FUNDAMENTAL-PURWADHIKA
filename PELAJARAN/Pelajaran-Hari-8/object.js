53ad0ff60574f4b5ba208fb6e9b9e82b6706ec7a
Kunto Mustiko <kuntomustiko@gmail.com>
2020-02-06 11:48:19 +0700

    materi object 06/02/2020

--- PELAJARAN/Pelajaran-Hari-8/object.js
+++ PELAJARAN/Pelajaran-Hari-8/object.js
@@ -31,3 +31,69 @@ console.log(person.pets)
 console.log(person.pets[1])
 person.intro()
 console.log()
+
+var person = {
+    name: 'John',
+    age: 79,
+    hobbies: ['Run', 'Chase'],
+    pets: [{
+        name: 'Holly'
+    }, {
+        name: 'Molly'
+    }],
+    // this disini untuk mengakses property yang berada di dalam object 
+    intro: function () {
+        console.log(`Hallo ${this.name}`)
+    }
+}
+
+// kita dapat memanggil object di dalam object
+// kita memanggil object coba di dalam method intro yang berada di dalam object person
+var person = {
+    name: 'John',
+    age: 79,
+    hobbies: ['Run', 'Chase'],
+    pets: [{
+        name: 'Holly'
+    }, {
+        name: 'Molly'
+    }],
+    // this disini untuk mengakses property yang berada di dalam object 
+    intro: function (obj) {
+        // console.log(`Hallo ${this.name}`)
+        obj.coba(this.name)
+    }
+}
+
+var coba = {
+    coba(x) {
+        console.log(`I got you ${x}`)
+    }
+}
+
+person.intro(coba)
+
+var person = {
+    name: 'John',
+    age: 79,
+    hobbies: ['Run', 'Chase'],
+    pets: [{
+        name: 'Holly'
+    }, {
+        name: 'Molly'
+    }, {
+        name: ['Ring', 'Ding', 'Dong']
+    }],
+    // this disini untuk mengakses property yang berada di dalam object 
+
+    intro: function () {
+        console.log(`Hello, My name is${this.name}`)
+        console.log(`My age is ${this.age} y.o`)
+        console.log(`One of my pet is ${this.pets[0].name}`)
+        console.log(`And the other one is, ${this.pets[1].name}`)
+        console.log(`Then i have ${this.pets[2].name}`)
+    }
+}
+
+console.log(person.name)
+person.intro()
