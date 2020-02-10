5096fd9b5917cf34c63fd39320810817e9577adb
Kunto Mustiko <kuntomustiko@gmail.com>
2020-02-10 13:24:36 +0700

    selesai tugas hari ke 11

---
+++ LATIHAN/Market/market-v7-guru.js
@@ -0,0 +1,33 @@
+class Products{
+    constructor(){
+        this.name = name
+        this.name = price
+        this.name = stock
+        this.name = qty
+        this.name = total
+    }
+}
+
+let products = [
+    {category: 1, name : 'Noodle', price: 20000, stock: 8, expired : 2020, qty : 0, total: 0},
+    {category: 2, name : 'Hoodie', price: 15000, stock: 7, size: 'L', qty : 0, total: 0},
+    {category: 3, name : 'Headphone', price: 20000, stock: 8, warranty : 'Yes', qty : 0, total: 0},
+    {category: 4, name : 'Apel', price: 10000, stock: 5, sugarLvl: 'High', qty : 0, total: 0},
+
+]
+
+function renderList() {
+    var listProducts = ""
+    if (products.length > 0) {
+        for (let index = 0; index < products.length; index++) {
+            listProducts +=
+
+        }
+
+        return listProducts
+    }
+
+    return ""
+}
+
+// shift + tab
